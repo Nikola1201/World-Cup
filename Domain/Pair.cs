d2ed4cc7c3380dff8f5a13f70edafa3c78f3d572
nikola-icic <ni20200393@student.fon.bg.ac.rs>
2024-05-15 00:45:36 +0200

    Added Domain classess Country and Pair

---
+++ Domain/Pair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain
+{
+    [Serializable]
+    public class Pair
+    {
+        public int Id { get; set; }
+        public Country HomeTeam { get; set; }
+        public Country AwayTeam { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
